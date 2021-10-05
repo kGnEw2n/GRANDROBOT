6f283e12d64d77e74fbf0212b7b271294d45bed9
kGnEw2n <91936591+kGnEw2n@users.noreply.github.com>
2021-10-05 12:21:16 +0600

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -142,9 +142,9 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/teamishere")],
-                                                                                   [InlineKeyboardButton(text="ADD GRAND OFFICIAL TO YOUR GROUP",
-                                                                       url="t.me/{}?startgroup=true".format(bot.username)),
+                                                                       url="t.me/s_mahmud_akash")],
+                                                                                   [InlineKeyboardButton(text="ADD SUKONNA TO YOUR GROUP",
+                                                                       url="t.me/{}?startgroup=true".format(bot.)),
                                                                                    InlineKeyboardButton(text="Source Code",
                                                                        url="https://github.com/legendx22/GRANDROBOT")
                                                                                  ]]))
