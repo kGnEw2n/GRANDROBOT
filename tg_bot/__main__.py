92ce06e23947cba5ff6ecefa7d1add8e98e9a87d
kGnEw2n <91936591+kGnEw2n@users.noreply.github.com>
2021-10-05 12:32:53 +0600

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -142,7 +142,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/s_mahmud_akash")],
+                                                                       url="t.me/S_Mahmud_Akash")],
                                                                                    [InlineKeyboardButton(text="ADD SUKONNA TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.)),
                                                                                    InlineKeyboardButton(text="Source Code",
@@ -150,7 +150,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                                  ]]))
 
     else:
-        update.effective_message.reply_text("Yuss, I am Already ONline")
+        update.effective_message.reply_text("bHai, BeChe AcHi EkHoNo 🙃")
 
 
 def send_start(bot, update):
